5568da62f30d5e90f33824b64e56f764b1ae1bd3
LucyMacko <118434024+LucyMacko@users.noreply.github.com>
2023-07-11 10:58:31 +0000

    done

--- src/models.py
+++ src/models.py
@@ -1,32 +1,61 @@
-import os
-import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship, declarative_base
+import enum
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum, Float
+from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
+
+class myEnum(enum.Enum):
+    refund = "refund"
+    paid = "paid"
+    cancelled = "cancelled"
+
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Product(Base):
+    __tablename__ = 'product'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True,)
+    name = Column(String(250), nullable=False)
+    pricing = Column(Float)
+    weight = Column(Float)
+    color = Column(String(250))    
+
+class Customer(Base):
+    __tablename__ = 'customer'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(250), unique= True)
+    address= Column(String(250))
 
-class Address(Base):
-    __tablename__ = 'address'
+class Shopping_Cart(Base):
+    __tablename__ = 'shopping_cart'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    product_id = Column(Integer, ForeignKey('product.id'), primary_key=True)
+    customer_id = Column(Integer, ForeignKey('customer.id'), primary_key=True)
+    quantity = Column(Integer)
+    price = Column(Float)
+    bill_id= Column(Integer, ForeignKey('bill.id'), primary_key=True)    
+
+class Bill(Base):
+    __tablename__ = 'bill'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, ForeignKey('shopping_cart.bill_id'), primary_key=True)
     street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    created_at = Column(Integer)
+    total_price = Column(Float)
+    status=Column(Enum(myEnum))    
 
     def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
+
+
